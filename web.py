b5d20915ade867969476a37db21a3d27a055c623
AaronDLF <aaron10lopfortich@gmail.com>
2025-11-10 21:01:42 -0500

    Add erase todo functionality

--- web.py
+++ web.py
@@ -1,19 +1,33 @@
 import streamlit as st
 import functions
+import os
+
+if not os.path.exists("todos.txt"):
+  with open("todos.txt","w") as file:
+    pass
+
 
 todos = functions.get_todos()
+
 
 def add_todo():
   todo = st.session_state["new_todo"] + "\n"
   todos.append(todo)
   functions.write_todos(todos)
+  st.session_state["new_todo"] = ""
 
 
 st.title("To-Do App")
 st.subheader("Manage your tasks easily")
 
-for todo in todos:
-  st.checkbox(todo)
+for index,todo in enumerate(todos):
+  checkbox = st.checkbox(todo,key=todo)
+  if checkbox:
+    todos.pop(index)
+    functions.write_todos(todos)
+    del st.session_state[todo]
+    st.rerun()
+
 
 
 st.text_input(label="", placeholder="Add a new to-do...", on_change=add_todo, key="new_todo")
